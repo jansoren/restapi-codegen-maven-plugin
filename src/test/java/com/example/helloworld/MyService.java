d80726f933d15a38ef0c40c67075b51d48521c55
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2016-07-21 23:53:37 +0200

    Started refactoring to jersey-client

--- src/test/java/com/example/helloworld/MyService.java
+++ src/test/java/com/example/helloworld/MyService.java
@@ -1,25 +1,41 @@
 package com.example.helloworld;
 
 import java.lang.Void;
-import no.bouvet.jsonclient.JsonClient;
 import no.jansoren.codegen.Something;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class MyService {
-  private final JsonClient jsonClient = new JsonClient();
+
+  private WebTarget target;
+
+  public MyService() {
+    Client client = ClientBuilder.newClient();
+    target = client.target("http://localhost:8080").path("something");
+  }
 
   public Something addSomething(Something dataToPost) {
-    return jsonClient.http().post("http://localhost:8080/something/add", dataToPost).object(Something.class);
+      Response response = target.path("add").request(MediaType.APPLICATION_JSON_TYPE).post(Entity.entity(dataToPost, MediaType.APPLICATION_JSON_TYPE));
+      return (Something)response.getEntity();
   }
 
   public Void deleteSomething() {
-    return jsonClient.http().delete("http://localhost:8080/something/delete").object(Void.class);
+      Response response = target.path("delete").request(MediaType.APPLICATION_JSON_TYPE).delete();
+      return (Void)response.getEntity();
   }
 
   public Something getSomething() {
-    return jsonClient.http().get("http://localhost:8080/something/get").object(Something.class);
+      Response response = target.path("get").request(MediaType.APPLICATION_JSON_TYPE).get();
+      return (Something)response.getEntity();
   }
 
-  public Something putSomething(Something dataToPost) {
-    return jsonClient.http().put("http://localhost:8080/something/put", dataToPost).object(Something.class);
+  public Something putSomething(Something dataToPut) {
+      Response response = target.path("get").request(MediaType.APPLICATION_JSON_TYPE).put(Entity.entity(dataToPut, MediaType.APPLICATION_JSON_TYPE));
+      return (Something)response.getEntity();
   }
 }
